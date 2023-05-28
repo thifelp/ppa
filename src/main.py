eab394a0d44df1d268561d4f13c26bf1e62f0d92
thifelp <thifelp@gmail.com>
2023-05-28 19:54:03 -0300

    primeira parte

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+#Open Doc and convert to CSV
+pd.read_excel(r'/home/thi/Dev/ppa/repository/Pasta.xlsx').to_csv('/home/thi/Dev/ppa/repository/Pasta.csv')
+
+# Read file csv
+data = pd.read_csv('/home/thi/Dev/ppa/repository/Pasta.csv')
+
+total = int(data.values[3][7])
+
+def mutually(arg1, arg2):
+    # Regra da Adição
+    #     184 + 164 = 348
+    #     ---   ---   ---
+    #     409   409   409
+    return print("%.2f" % ((arg1 + arg2) / total))
+
+def mutuallyExclusive(arg1, arg2, arg3):
+
+    # Regra da Adição
+    #     arg1 + arg2 - arg3 = soma
+    #     ----   ----   ----   ----
+    #     total  total  total  total
+    return print("%.2f" % (((arg1 + arg2) - arg3) / total))
+
+
+print("1 - Qual a probabilidade de que o doador tenha sangue do tipo O ou tipo A?")
+mutually(184, 65)
+print("2 - Qual a probabilidade de que o doador tenha sangue tipo B ou que seja Rh negativo? ")
+mutuallyExclusive(45, 65, 8)
+
